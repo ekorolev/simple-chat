3c624aa930a34c87b68ee95433cd9abe9ab302f5
Evgenii K <aksyline@gmail.com>
2020-03-28 06:40:22 +0300

    deleted test code

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,13 +9,6 @@ import mutations from './store/mutations'
 
 export default {
   created () {
-    setTimeout(() => {
-      console.log(this.$socket)
-      this.$socket.close()
-      setTimeout(() => {
-        this.$socket.connect()
-      }, 2000)
-    }, 4000)
     window.onfocus = () => {
       console.log('window focused')
       this.$store.commit(mutations.SET_WINDOW_FOCUSED, true)
